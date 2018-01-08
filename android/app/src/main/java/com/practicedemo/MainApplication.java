6d75b3a618fd4f360e015f5e53b9528920bb36a6
刘超 <liuchaofeichen@163.com>
2018-01-08 17:26:02 +0800

    添加mpAndroid chart

--- android/app/src/main/java/com/practicedemo/MainApplication.java
+++ android/app/src/main/java/com/practicedemo/MainApplication.java
@@ -3,6 +3,7 @@ package com.practicedemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.wuxudong.rncharts.MPAndroidChartPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
                     new SvgPackage(),
-                    new MyViewPackage()
+                    new MyViewPackage(),
+                    new MPAndroidChartPackage()
             );
         }
     };
